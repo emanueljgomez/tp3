15e1931ad530f917b6c60c004e173f74b3974c6c
Emanuel Gomez <emanueljgomez@gmail.com>
2021-06-10 14:56:06 -0300

    Created Array 'productos' at home.component.ts -- Created ngFor iteration for said array at home.component.html

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,8 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  // Crear Array para mostrar
+
+  productos=[
+    "iPhone X",
+    "Moto G",
+    "iPhone 12"
+  ]
+
+
   constructor() { }
 
+
+  
   ngOnInit(): void {
   }
 
